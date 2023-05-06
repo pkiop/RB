bb4f36b898dea3ab9035eb955a1c9098b7759b75
pkiop <pkiopb@gmail.com>
2023-05-06 15:14:07 +0900

    add get_posts

--- static-generator/src/main.rs
+++ static-generator/src/main.rs
@@ -4,9 +4,9 @@ mod model;
 async fn main() {
     println!("Hello, world!");
     dotenv::dotenv().ok();
-    let document = mongodb::bson::doc! {
-        "title": "Hello, world!",
-        "content": "This is my first post!",
-    };
-    model::posts::add_post(document).await;
+    // let document = mongodb::bson::doc! {
+    //     "title": "Hello, world!",
+    //     "content": "This is my first post!",
+    // };
+    model::posts::get_posts().await;
 }
--- static-generator/src/model/posts.rs
+++ static-generator/src/model/posts.rs
@@ -1,5 +1,13 @@
 use super::core::get_mongodb_client;
+use futures::stream::TryStreamExt;
 use mongodb::{bson::Document, Collection};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Post {
+    name: String,
+    content: String,
+}
 
 pub async fn get_post_collection() -> Collection<Document> {
     let client = get_mongodb_client().await.unwrap();
@@ -15,5 +23,15 @@ pub async fn add_post(document: Document) {
         .await
         .expect("insert failed");
     println!("inserting success");
+    return;
+}
+
+pub async fn get_posts() {
+    let post_collection = get_post_collection().await;
+    let mut cursor = post_collection.find(None, None).await.unwrap();
+    while let Some(post_doc) = cursor.try_next().await.unwrap() {
+        let post: Post = bson::from_document(post_doc).unwrap();
+        println!("title: {}", post.name);
+    }
     return;
 }
